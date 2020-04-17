bbe0e8a6b58c477899c6500510a46f32c8e28139
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-17 16:22:55 -0700

    Wrote the headerDiv, dateSpan, timesHeader, and tempSpan in the function, returning the headerDiv, and then appending the function to the headContainer.

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,15 +2,35 @@
 // -----------------------
 // Using a function create the component you see below:
 //
-//  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
-//    <h1>Lambda Times</h1>
-//    <span class="temp">98°</span>
-//  </div >
 // And add it to the DOM in the .header-container component
 
 function Header() {
+    //  <div class="header">
+    let headerDiv = document.createElement('div');
+    headerDiv.classList.add('header');
     
+    //    <span class="date">SMARCH 28, 2019</span>
+    let dateSpan = document.createElement('span');
+    dateSpan.classList.add('date');
+    dateSpan.textContent = 'SMARCH 28, 2019';
+    headerDiv.appendChild(dateSpan);
+    
+    //    <h1>Lambda Times</h1>
+    let timesHeader = document.createElement('h1');
+    timesHeader.textContent = 'Lambda Times';
+    headerDiv.appendChild(timesHeader);
+
+    //    <span class="temp">98°</span>
+    let tempSpan = document.createElement('span');
+    tempSpan.classList.add('temp');
+    tempSpan.textContent = '98\*';
+    headerDiv.appendChild(tempSpan);
+    //  </div >
+
+
+    return headerDiv
 }
 
 const headContainer = document.querySelector('.header-container');
+headContainer.appendChild(Header());
+console.log(headContainer);
