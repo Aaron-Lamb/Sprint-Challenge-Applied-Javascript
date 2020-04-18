1e7b90c62226f01b7cc158861a3ec8956663b037
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-17 17:39:54 -0700

    Created an articles array, then created my get request. In my .then() method I isolated the object, and then its' values, so that I could iterate through them. Within that forEach, I looped through the values again, selected each item and pushed it to articlesArray. I selected the card container, and did a forEach on the articlesArray, creating my cards and appended them to the card container.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,14 +6,52 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //
-// <div class="card">
-//   <div class="headline">{Headline of article}</div>
-//   <div class="author">
-//     <div class="img-container">
-//       <img src={url of authors image} />
-//     </div>
-//     <span>By {authors name}</span>
-//   </div>
-// </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+let articlesArray = []
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then((response) => {
+    console.log(response.data.articles);
+    let articleObject = response.data.articles;
+    let keys = Object.values(articleObject);
+    keys.forEach((item) => {
+        for (let index = 0; index < item.length; index++) {
+            articlesArray.push(item[index]);
+        }
+    })
+    const cardContainer = document.querySelector('.cards-container');
+        articlesArray.forEach((article) => {
+            // <div class="card">
+            let cardDiv = document.createElement('div');
+            cardDiv.classList.add('card');
+            //   <div class="headline">{Headline of article}</div>
+            let headlineDiv = document.createElement('div');
+            headlineDiv.classList.add('headline');
+            headlineDiv.textContent = article.headline;
+            cardDiv.appendChild(headlineDiv);
+            //   <div class="author">
+            let authorDiv = document.createElement('div');
+            authorDiv.classList.add('author');
+            cardDiv.appendChild(authorDiv);
+            //     <div class="img-container">
+                    let imgDiv = document.createElement('div');
+                    imgDiv.classList.add('img-container');
+                    authorDiv.appendChild(imgDiv);
+            //       <img src={url of authors image} />
+                            let authImg = document.createElement('img');
+                            authImg.src = article.authorPhoto;
+                            imgDiv.appendChild(authImg);
+            //     </div>
+            //     <span>By {authors name}</span>
+                    let nameSpan = document.createElement('span');
+                    nameSpan.textContent = `By ${article.authorName}`;
+                    authorDiv.appendChild(nameSpan);
+            //   </div>
+            // </div>
+            cardContainer.appendChild(cardDiv);
+            })
+})
+.catch(error => {
+    console.log(error)
+})
